64a94da9a1dedad65a341ce8d71ed395451ab741
TitouanUCO <45257028+TitouanUCO@users.noreply.github.com>
2018-11-22 10:47:02 +0100

    Add files via upload

--- MIB_exos.py
+++ MIB_exos.py
@@ -4,3 +4,7 @@ from MaBase_MIB import *
 
 ### Question 1 : quel est l'ensemble des gardiens?
 les_gardiens = {gardien.Nom for gardien in BaseGardiens}
+les_villes_agents = {agent.Ville for agent in BaseAgents}
+triples = { (alien.NoCabine, alien.Nom, gardien.Nom) for alien in BaseAliens  for gardien in BaseGardiens if gardien.NoCabine == alien.NoCabine}
+
+couples={(alien.Nom,cabine.NoAllee) for alien in BaseAliens for cabine in BaseAliens if cabine.NoCabine == alien.NoCabine}
